77710c6db5b54fdf66d990515c686ccf62098fc4
xoxwgys56 <lololtoday@gmail.com>
2021-11-27 00:42:28 +0900

    chore: update progress

--- velopert/begin-react/src/App.js
+++ velopert/begin-react/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hello from "./Hello";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const name = "react";
+    const style = {
+        background: "black",
+        color: "aqua",
+        fontSize: 24,
+        padding: "1rem"
+    };
+
+    return (
+        <>
+            <Hello name={name}/>
+            <div style={style}>{name}</div>
+            <div className="gray-box"></div>
+        </>
+    );
 }
 
 export default App;
