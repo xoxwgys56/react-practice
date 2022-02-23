096f73412743c1b1a3b16593da2490e8f7245539
xoxwgys56 <lololtoday@gmail.com>
2022-02-23 22:10:52 +0900

    chore: apply useSelector

--- digitalocean-redux/src/index.js
+++ digitalocean-redux/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { createStore } from "react-redux";
+import { createStore } from "redux";
 
 const store = createStore(() => ({
     birds: [
